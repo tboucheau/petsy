cae6860c3e9bab4cda0c5bad52dd95d2b7b1301e
tboucheau <opposite240@gmail.com>
2016-12-28 20:37:04 +0100

    sur confirmation_token secure_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     has_secure_password
-    has_secure_token
+    has_secure_token :confirmation_token
     validates :username,
         format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumériques ou des _'},
         uniqueness: {case_sensitive: false}
