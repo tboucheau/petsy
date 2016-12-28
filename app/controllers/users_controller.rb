4a95186df9fa33807657572158825788c939a8e1
tboucheau <opposite240@gmail.com>
2016-12-28 23:14:28 +0100

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
         if @user.confirmation_token == params[:token]
             @user.update_attributes(confirmed: true, confirmation_token: nil)
             @user.save(validate: false)
-            session[:auth] = {id: @user.id}
+            session[:auth] = @user.to_session
             redirect_to profil_path, success: 'Votre compte a bien été confirmé'
         else
             redirect_to new_user_path, danger: 'Ce token ne semble pas valide'
