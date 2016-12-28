3c20f7452f41bd8e9590b2d95ceb5333521fc1fe
tboucheau <opposite240@gmail.com>
2016-12-28 19:41:26 +0100

    configuration de base

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Petsy
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+        g.assets false
+        g.helper false
+        g.test_framework false
+        g.jbuilder false
+    end
   end
 end
