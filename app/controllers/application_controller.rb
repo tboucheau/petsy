d2a6caa74aa9983f6bce84e066ad06103424c081
tboucheau <opposite240@gmail.com>
2016-12-29 00:10:27 +0100

    menu déroulant quand utilisateur est connecté

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
 
     add_flash_types :success, :danger
 
-    helper_method :current_user
+    helper_method :current_user, :user_signed_in?
 
     private
     def only_signed_in
-        if !current_user
+        if !user_signed_in?
             redirect_to new_user_path, danger: "Vous n'avez pas le droit d'accéder à cette page"
         end
     end
@@ -20,5 +20,9 @@ class ApplicationController < ActionController::Base
         return nil if !session[:auth] || !session[:auth]['id']
         return @user if @user
         @user = User.find_by_id(session[:auth]['id'])
+    end
+
+    def user_signed_in?
+        current_user.nil?
     end
 end
