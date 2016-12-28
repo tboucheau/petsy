cb096ec98760f229e6e6e516dd4d564d76f1bfda
tboucheau <opposite240@gmail.com>
2016-12-28 20:30:43 +0100

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :username,
-        format: {with: /\A[a-zA-Z0-9_]{2,20}\z/}, message: 'ne doit contenir que des caractères alphanumériques ou des underscores'
+        format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumériques ou des _'},
         uniqueness: {case_sensitive: false}
 
     validates :email,
-        format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/}
+        format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/},
         uniqueness: {case_sensitive: false}
 
 end
