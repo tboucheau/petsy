09a4bb947de634aeb9437a201eed2e5d9d22640b
tboucheau <opposite240@gmail.com>
2016-12-28 20:28:34 +0100

    validation des usernames et emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
 
     has_secure_password
+    validates :username,
+        format: {with: /\A[a-zA-Z0-9_]{2,20}\z/}, message: 'ne doit contenir que des caractères alphanumériques ou des underscores'
+        uniqueness: {case_sensitive: false}
+
+    validates :email,
+        format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/}
+        uniqueness: {case_sensitive: false}
 
 end
