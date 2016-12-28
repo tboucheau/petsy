0c55f1f7a950e0b0c93f43e3a68da3d9627a08ca
tboucheau <opposite240@gmail.com>
2016-12-28 20:36:40 +0100

    secure token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
     has_secure_password
+    has_secure_token
     validates :username,
         format: {with: /\A[a-zA-Z0-9_]{2,20}\z/, message: 'ne doit contenir que des caractères alphanumériques ou des _'},
         uniqueness: {case_sensitive: false}
