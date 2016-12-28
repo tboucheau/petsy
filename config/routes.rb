5629d12ae13fa494b56e57521cddfdcf6541d249
tboucheau <boucheau.tony@neuf.fr>
2016-12-28 21:58:08 +0000

    gestion des utilisateurs connees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
     root to: 'users#new'
     get '/profil', to: 'users#edit', as: :profil
     patch '/profil', to: 'users#update'
