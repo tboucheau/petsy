9daf76985a03d6284318dd3ae2811daf45bf195a
tboucheau <opposite240@gmail.com>
2016-12-28 23:17:39 +0100

    confirmed true

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
       user_params = params.require(:user)
-      @user = User.where(username: user_params[:username]).or(User.where(email: user_params[:email])).first
+      @user = User.where(username: user_params[:username], confirmed: true).or(User.where(email: user_params[:email]), confirmed: true).first
       if @user and @user.authenticate(user_params[:password])
         session[:auth] = @user.to_session
           redirect_to profil_path, success: 'Connection réussie'
