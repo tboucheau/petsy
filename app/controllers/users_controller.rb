30f6b82bad274228de2fe618068500092517bade
tboucheau <opposite240@gmail.com>
2016-12-28 20:19:26 +0100

    Controller et gem bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        @user = User.new(user_params)
+        if @user.valid?
 
+        else
+            render 'new'
+        end
     end
 
 end
