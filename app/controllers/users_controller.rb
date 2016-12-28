6098a712bf50148812158cc0d7229e0debb7870e
tboucheau <opposite240@gmail.com>
2016-12-28 20:34:07 +0100

    debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
         @user = User.new(user_params)
         if @user.valid?
-
+            render 'new'
         else
             render 'new'
         end
