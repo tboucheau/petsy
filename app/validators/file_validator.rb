44e6d4b911e88af6a66a745a25409d4125017edc
tboucheau <opposite240@gmail.com>
2016-12-29 00:50:23 +0100

    test

--- app/validators/file_validator.rb
+++ app/validators/file_validator.rb
@@ -4,7 +4,7 @@ class FileValidator < ActiveModel::EachValidator
         if value
             if value.respond_to? :path
                 unless options[:ext].include? File.extname(value.path).delete('.').to_sym
-                    record.errors[attributes] << "is not a valid file (#{options[:ext].join(',')})"
+                    record.errors[attribute] << "is not a valid file (#{options[:ext].join(',')})"
                 end
             else
                 record.errors[attribute] << (options[:message] || 'is not a file')
