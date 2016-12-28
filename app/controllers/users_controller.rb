2f34603bd7ee4d5dd2aa2647d8e0ed56158ba900
tboucheau <opposite240@gmail.com>
2016-12-28 22:41:48 +0100

    typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
     def confirm
         @user = User.find(params[:id])
-        if @user.confirmation_token == params[:confirmation_token]
+        if @user.confirmation_token == params[:token]
             @user.update_attributes(confirmed: true, confirmation_token: nil)
             @user.save(validate: false)
             session[:auth] = {id: @user.id}
