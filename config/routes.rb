0cbefe208040512315cace746254d0f326913c22
tboucheau <boucheau.tony@neuf.fr>
2016-12-29 22:14:07 +0000

    Scafflod +migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts
     root to: 'users#new'
 
     get '/profil', to: 'users#edit', as: :profil
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229022121) do
+ActiveRecord::Schema.define(version: 20161229221314) do
 
   create_table "pets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20161229022121) do
     t.datetime "updated_at", null: false
     t.index ["species_id"], name: "index_pets_on_species_id", using: :btree
     t.index ["user_id"], name: "index_pets_on_user_id", using: :btree
+  end
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "name"
+    t.text     "content",    limit: 65535
+    t.integer  "user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "species", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 20161229022121) do
 
   add_foreign_key "pets", "species"
   add_foreign_key "pets", "users"
+  add_foreign_key "posts", "users"
 end
