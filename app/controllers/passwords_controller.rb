7fe10a9f35c098043792e352f50a3dd68cabd88d
tboucheau <opposite240@gmail.com>
2016-12-29 01:50:18 +0100

    password update

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,7 +7,6 @@ class PasswordsController < ApplicationController
     end
 
     def create
-        user_params = params.require(:user)
         @user = User.find_by_email(user_params[:email])
         if @user
             @user.regenerate_recover_password
@@ -16,5 +15,31 @@ class PasswordsController < ApplicationController
         else
            redirect_to new_password_path, danger: 'Aucun utilisateur ne correspond à cet email'
         end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        if @user.recover_password != params[:token]
+            redirect_to new_password_path, danger: 'Token invalide'
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.recover_password === user_params[:recover_password]
+            @user.assign_attributes(user_params)
+            if @user.valid?
+               @user.recover_password = nil
+               @user.save
+               session[:auth]
+               redirect_to profil_path, success: 'Votre mot de passe a bien été modifié'
+            end
+        else
+            redirect_to new_password_path, danger: 'Token invalide'
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:password, :password_confirm)
     end
 end
