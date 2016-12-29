47eaf59d6a0e8d34c95b50a26676b904688359b2
tboucheau <opposite240@gmail.com>
2016-12-29 17:12:48 +0100

    taille par défault

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,7 +7,7 @@ class Pet < ApplicationRecord
     validates :avatar_file, presence: true, on: :create
     validate :birthday_not_future
 
-    has_image :avatar, resize: 500
+    has_image :avatar, resize: 150
 
     def age
         Time.now.year - birthday.year
