86a716b4c562d94a42598ca27f250c3262d3b8f8
tboucheau <opposite240@gmail.com>
2016-12-29 03:22:36 +0100

    modif migration pets

--- db/migrate/20161229022121_create_pets.rb
+++ db/migrate/20161229022121_create_pets.rb
@@ -2,7 +2,7 @@ class CreatePets < ActiveRecord::Migration[5.0]
   def change
     create_table :pets do |t|
       t.string :name
-      t.string :gender
+      t.string :gender, length: 1
       t.date :birthday
       t.references :user, foreign_key: true
       t.references :species, foreign_key: true
