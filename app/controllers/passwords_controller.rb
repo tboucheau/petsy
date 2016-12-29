a74413a7fcdabc2985fbb0ee55015e50eb221574
tboucheau <opposite240@gmail.com>
2016-12-29 02:45:31 +0100

    enfin !!! j'ai vraiment pas les yeux en face des trous

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,13 +8,13 @@ class PasswordsController < ApplicationController
 
     def create
         user_params = params.require(:user)
-        @user = User.find_by_email(user_params)
+        @user = User.find_by_email(user_params[:email])
         if @user
             @user.regenerate_recover_password
             UserMailer.password(@user).deliver_now
             redirect_to new_session_path, success: 'Un email vous a été envoyé pour réinitialiser votre mot de passe'
         else
-           redirect_to new_password_path, danger: 'Aucun utilisateur ne correspond à cet email'
+            redirect_to new_password_path, danger: 'Aucun utilisateur ne correspond à cet email'
         end
     end
 
