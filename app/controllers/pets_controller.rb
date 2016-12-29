840c0b61490ea1d5386efdbda9e8c412668ca53d
tboucheau <opposite240@gmail.com>
2016-12-29 03:56:00 +0100

    formulaire ajout animal

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,4 +4,21 @@ class PetsController < ApplicationController
         @pets = current_user.pets
     end
 
+    def new
+        @pet = current_user.pets.new
+    end
+
+    def create
+        @pet = current_user.pets.new(pet_params)
+        if @pet.save
+            redirect_to pets_path, success: 'Votre animal a bien été créé'
+        else
+            render :new
+        end
+    end
+
+    private
+    def pet_params
+        params.require(:pet).permit(:name, :gender, :birthday)
+    end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,15 @@
 class Pet < ApplicationRecord
-  belongs_to :user
-  belongs_to :species
+    belongs_to :user
+    belongs_to :species
+
+    validates :name, :gender, :birthday, presence: true
+    validates :gender, format: {with: /\A(M|F)\z/}
+    validates :birthday_not_future
+
+
+    def birthday_not_future
+        if birthday.present? && birthday.future?
+            errors.add(:birthday, 'ne peut être dans le futur')
+        end
+    end
 end
