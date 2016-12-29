eb9afe383c9f425ea00dcd52884f270304831d31
tboucheau <opposite240@gmail.com>
2016-12-29 14:35:23 +0100

    avatar url

--- app/controllers/concerns/image_concern.rb
+++ app/controllers/concerns/image_concern.rb
@@ -6,6 +6,14 @@ module ImageConcern
         def has_image(field, options = {})
             attr_accessor "#{field}_file".to_sym
             validates "#{field}_file".to_sym, file: {ext: [:jpg, :png]}
+            #before_save "#{field}_file_before_upload".to_sym
+            #after_save "#{field}_file_after_upload".to_sym
+            #after_destroy "#{field}_file_commit _destroy".to_sym
+
+            class_eval <<- METHODS
+
+
+            METHODS
         end
     end
 
