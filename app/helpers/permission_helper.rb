b138a390a009e5c2c39d6e6f08f0feb2080fb511
tboucheau <opposite240@gmail.com>
2016-12-30 13:34:52 +0100

    typo

--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -1,7 +1,7 @@
 module PermissionHelper
 
     def canEdit(record)
-        if record.respond_to?(:user_id) and user_signed_in? and record.user_id === current_user_id
+        if record.respond_to?(:user_id) and user_signed_in? and record.user_id === current_user.id
             true
         else
             false
