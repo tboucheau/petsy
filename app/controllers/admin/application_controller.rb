de1bda9ef02cbddfbbe1417cf261165824d0aa37
tboucheau <opposite240@gmail.com>
2016-12-31 11:29:23 +0100

    restriction des pages admins

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,13 @@ module Admin
     class ApplicationController < ::ApplicationController
 
     #layout 'admin' -- pour faire un layout différent en administrateur
+    befor_filter :only_admin
 
+    private
+        def only_admin
+            if !user_signed_in? || current_user.role != 'admin'
+                redirect_to new_user_path, danger: "Vous n'avez pas le droit d'accéder à cette page"
+            end
+        end
     end
 end
