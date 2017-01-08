3ffa2fcb1a05a60d5244d366ce9b24654ac03d8d
tboucheau <opposite240@gmail.com>
2017-01-08 01:16:22 +0100

    test cache dans le controlleur

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
     skip_before_action :only_signed_in
 
     def index
-        @posts = Post.limit(10)
+        @posts = Rails.cache.fetch('posts_homepage') do
+            Post.limit(10).all.to_a
+        end
     end
 end
